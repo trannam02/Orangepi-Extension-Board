2f5b26f94d19191e269e93e9ac8839f881e3b483
Tran Xuan Nam <tranxuannam02@gmail.com>
2026-05-02 08:55:02 +0700

    add gitignore to remove unnescessary files

--- Debug/Core/Src/knx.d
+++
@@ -1,2 +0,0 @@
-Core/Src/knx.o: ../Core/Src/knx.c ../Core/Inc/knx.h
-../Core/Inc/knx.h:
--- Debug/Core/Src/queue_utils.d
+++
@@ -1,3 +0,0 @@
-Core/Src/queue_utils.o: ../Core/Src/queue_utils.c \
- ../Core/Inc/queue_utils.h
-../Core/Inc/queue_utils.h:
